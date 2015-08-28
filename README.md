3edd9d1814f594d81a966a9dce8bc0fd60c11a0f
GabMus <emaildigabry@gmail.com>
2015-08-28 21:22:00 +0200

    implemented "open" and "save as"

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 
 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 
-![mLauncherScrot](http://i.imgur.com/D3E3xeH.png)
+![mLauncherScrot](http://i.imgur.com/eDyDGNt.png)
--- main.py
+++ main.py
@@ -2,12 +2,7 @@
 from gi.repository import Gtk
 from gi.repository import GdkPixbuf
 import os
-from os import path as ospath
-from os import environ as osenviron
-from os import remove as osremove
 import stat
-from os import stat as osstat
-from os import chmod as oschmod
 
 
 builder = Gtk.Builder()
@@ -15,12 +10,63 @@ builder.add_from_file("ui.glade")
 HOME=os.environ.get('HOME')
 DEFAULT_SAVE_PATH=HOME+"/.local/share/applications/"
 
+listbox_recents = builder.get_object("listboxRecents")
+
+#recents manager
+recent_manager = Gtk.RecentManager()
+recents = recent_manager.get_items()
+recents_count = len(recents)
+act_recents_count = 0
+desktop_recents_path = []
+desktop_recents_name = []
+#get_row_at_index
+def refreshRecents():
+	global act_recents_count
+	global desktop_recents_name
+	global desktop_recents_name
+	global recents
+	recents = recent_manager.get_items()
+	j=0
+	while j<act_recents_count:
+		listbox_recents.remove(listbox_recents.get_row_at_index(0))
+		j+=1
+	i=0
+	desktop_recents_path = []
+	desktop_recents_name = []
+	while i<recents_count and len(desktop_recents_path) < 5:
+		if recents[i].get_uri()[-8:] == ".desktop" and recents[i].get_uri()[:7]=="file://":
+			desktop_recents_path.append(recents[i].get_uri()[7:])
+			desktop_recents_name.append(recents[i].get_display_name())
+		i+=1
+	act_recents_count=len(desktop_recents_name)
+	i=0
+	while i<act_recents_count:
+		box= Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+		labelName= Gtk.Label()
+		labelPath= Gtk.Label()
+		labelName.set_xalign(0)
+		labelPath.set_xalign(0)
+		labelPath.set_opacity(0.5)
+		box.set_spacing(6)
+		labelName.set_text(desktop_recents_name[i])
+		labelPath.set_text(desktop_recents_path[i])
+		box.pack_end(labelPath, True, True, 0)
+		box.pack_end(labelName, True, True, 0)
+		row=Gtk.ListBoxRow()
+		row.add(box)
+		listbox_recents.add(row)
+		i+=1
+	
+refreshRecents()
+
+
 class Handler:
 
 	iconPath="icon"
 	infobarButtonsActionCode=0
 	savePath=DEFAULT_SAVE_PATH+"default-mLauncher.desktop"
-	
+	untouchableLines= "\n"
+	customSavePath= None
 
 	def __init__(self):
 		self.updateRadios()
@@ -75,7 +121,7 @@ class Handler:
 		name=builder.get_object("entryName").get_text().strip()
 		executable=builder.get_object("entryExec").get_text().strip()
 		path=builder.get_object("entryPath").get_text().strip()
-		category=builder.get_object("comboboxtextCategory").get_active_text()
+		category=builder.get_object("comboboxtext-entry").get_text()
 		terminal=builder.get_object("switchTerminal").get_state()
 		#check if the name contains nothing or only spaces
 		if name and executable:
@@ -97,7 +143,11 @@ class Handler:
 					launcherString+="Path="+path+"\n"
 				launcherString+="Name="+name+"\n"
 				launcherString+="Icon="+self.iconPath
-				self.savePath=DEFAULT_SAVE_PATH+name+"-mLauncher.desktop"
+				launcherString+=self.untouchableLines
+				if self.customSavePath:
+					self.savePath=self.customSavePath
+				else:
+					self.savePath=DEFAULT_SAVE_PATH+name+".desktop"
 				
 				if not os.path.isfile(self.savePath):
 					out_file = open(self.savePath,"w")
@@ -105,10 +155,11 @@ class Handler:
 					out_file.close()
 					st= os.stat(self.savePath)
 					os.chmod(self.savePath, st.st_mode | stat.S_IEXEC)
+					recent_manager.add_item("file://"+self.savePath)
 					print("\n\nDebug:\n"+launcherString)
-					self.showInfo("File saved in the default launchers path (~/.local/share/applications/)", False)
+					self.showInfo("File saved ("+self.savePath+")", False)
 				else:
-					#the infobar has to replace and save
+					#the infobar has to delete the file and recall this method
 					self.showInfo("The file "+self.savePath+" already exists. Do you want to replace it?", True, 1)
 		else:
 			self.showInfo("Missing name and/or executable! Fix it!", False)
@@ -126,16 +177,14 @@ class Handler:
 	def on_filechooserbuttonExecutable_file_set(self, button):
 		entry_exec=builder.get_object("entryExec")
 		execFileName=button.get_filename()
-		i=0
 		processedFileName=""
-		while i<len(execFileName):
+		for i in execFileName:
 			if execFileName[i] == " ":
 				processedFileName+="\\ "
 			elif execFileName[i] == "\\":
 				processedFileName+="\\\\"
 			else:
 				processedFileName+=execFileName[i]
-			i+=1
 		entry_exec.set_text(processedFileName)
 		
 	def on_filechooserbuttonPath_file_set(self, button):
@@ -155,7 +204,149 @@ class Handler:
 			self.buttonSave_clicked_cb(button)
 		else:
 			print("Infobar action code was "+self.infobarButtonsActionCode+". Tell the programmer something's wrong!")
+			
+	def on_buttonOpen_clicked(self, button):
+		if popover.get_visible():
+			popover.hide()
+		else:
+			refreshRecents()
+			popover.show_all()
+			
+	def resetUI(self):
+		builder.get_object("entryName").set_text("")
+		builder.get_object("entryExec").set_text("")
+		builder.get_object("entryPath").set_text("")
+		builder.get_object("comboboxtext-entry").set_text("")
+		builder.get_object("switchTerminal").set_state(False)
+			
+	def processFile(self, path):
+		try:
+			with open(path) as f:
+				lines = f.readlines()
+		
+			self.customSavePath=path
+		
+			name=builder.get_object("entryName")
+			executable=builder.get_object("entryExec")
+			path=builder.get_object("entryPath")
+			category=builder.get_object("comboboxtext-entry")
+			terminal=builder.get_object("switchTerminal")
+		
+			self.resetUI()
+		
+			i=0
+			self.untouchableLines= "\n"
+			skip = [False, False, False, False, False, False]
+			while len(lines)>0:
+				if lines[i].strip() == "[Desktop Entry]":
+					lines.pop(i)
+				elif lines[i].strip()[:9] == "Encoding=":
+					lines.pop(i)
+				elif lines[i].strip() == "Type=Application":
+					lines.pop(i)
+				elif lines[i].strip()[:9] == "Terminal=" and not skip[0]:
+					if "true" in lines[i]:
+						terminal.set_state(True)
+					else:
+						terminal.set_state(False)
+					lines.pop(i)
+					skip[0]=True
+				elif lines[i].strip()[:5] == "Exec=" and not skip[1]:
+					executable.set_text(lines[i].strip()[5:])
+					lines.pop(i)
+					skip[1]=True
+				elif lines[i].strip()[:5] == "Name=" and not skip[2]:
+					name.set_text(lines[i].strip()[5:])
+					lines.pop(i)
+					skip[2]=True
+				elif lines[i].strip()[:5] == "Name[":
+					lines.pop(i)
+				elif lines[i].strip()[:5] == "Icon=" and not skip[3]:
+					mIcon=lines[i].strip()[5:]
+					print(os.path.isfile(mIcon))
+					if os.path.isfile(mIcon):
+						builder.get_object("radiobuttonIconPath").set_active(True)
+						builder.get_object("radiobuttonIconName").set_active(False)
+						builder.get_object("filechooserbuttonIconPath").set_filename(mIcon)
+						#self.updateRadios()
+					else:
+						builder.get_object("radiobuttonIconPath").set_active(False)
+						builder.get_object("radiobuttonIconName").set_active(True)
+						builder.get_object("entryIconName").set_text(mIcon)
+						#self.updateRadios()
+						self.on_buttonLoadIconByName_clicked(builder.get_object("buttonLoadIconByName"))
+					self.iconPath=mIcon
+					lines.pop(i)
+					skip[3]=True
+				elif lines[i].strip()[:5] == "Path=" and not skip[4]:
+					path.set_text(lines[i].strip()[5:])
+					lines.pop(i)
+					skip[4]=True
+				elif lines[i].strip()[:11] == "Categories=" and not skip[5]:
+					category.set_text(lines[i].strip()[11:])
+					lines.pop(i)
+					skip[5]=True
+				else:
+					self.untouchableLines+=lines.pop(i)
+		except:
+			self.customSavePath= None
+			print("ERR: File not found")
+			self.showInfo("The file does not exist! Maybe you deleted it?", False)
+		popover.hide()
+									
+		
+			
+	def on_listboxRecents_row_activated(self, listbox, row):
+		selectedFilePath=row.get_children()[0].get_children()[1].get_text()
+		self.processFile(selectedFilePath)
+		
+	def on_buttonOpenBrowse_clicked(self, button):
+		builder.get_object("filechooserdialogOpenBrowse").set_current_folder_uri("file://"+HOME)
+		builder.get_object("filechooserdialogOpenBrowse").show_all()
+		popover.hide()
+		
+	
+	def on_buttonDesktopChooserOk_clicked(self, button):
+		self.on_filechooserdialogOpenBrowse_file_activated(builder.get_object("filechooserdialogOpenBrowse"))
+		
+		
+	def on_filechooserdialogOpenBrowse_file_activated(self, dialog):
+		if dialog.get_filename()[-8:] == ".desktop":
+			self.processFile(dialog.get_filename())
+		else:
+			self.showInfo("The file selected is not valid. Look for one with a .desktop extension.", False)
+		dialog.hide()
+	
+	
+	def on_buttonDesktopChooserCancel_clicked(self, button):
+		builder.get_object("filechooserdialogOpenBrowse").hide()
+	
+	def on_buttonSaveAsChooserCancel_clicked(self, button):
+		builder.get_object("filechooserdialogSaveAs").hide()
+	
+	def on_buttonDesktopChooserOpenDefaultFolder_clicked(self, button):
+		builder.get_object("filechooserdialogOpenBrowse").set_current_folder_uri("file://"+HOME+"/.local/share/applications")
+		
+	def on_buttonSaveAsChooserOpenDefaultFolder_clicked(self, button):
+		builder.get_object("filechooserdialogSaveAs").set_current_folder_uri("file://"+HOME+"/.local/share/applications")
+		
+	def on_buttonSaveAs_clicked(self, button):
+		builder.get_object("filechooserdialogSaveAs").set_current_folder_uri("file://"+HOME)
+		builder.get_object("filechooserdialogSaveAs").show_all()
+		
+	def on_buttonSaveAsChooserOk_clicked(self, button):
+		path=builder.get_object("filechooserdialogSaveAs").get_filename()
+		if path[-8:] != ".desktop":
+			path+=".desktop"
+		self.customSavePath=path
+		builder.get_object("filechooserdialogSaveAs").hide()
+		self.buttonSave_clicked_cb(button)
+		
+			
+			
 
+popover = Gtk.Popover.new(builder.get_object("buttonOpen"))
+popover.add(builder.get_object("openerWidget"))
 builder.connect_signals(Handler())
 window = builder.get_object("window1")
 window.show_all()
