2e43883522b029d0094fca9ccf7fa463167b6b85
GabMus <emaildigabry@gmail.com>
2015-08-30 18:39:26 +0200

    added appmenu, about dialog implemented correct gtkapplication class

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-from gi.repository import Gtk
+from gi.repository import Gtk, Gio
 from gi.repository import GdkPixbuf
 import os
 import stat
+import sys
 
 
 builder = Gtk.Builder()
@@ -58,6 +59,39 @@ def refreshRecents():
 		i+=1
 	
 refreshRecents()
+
+class App(Gtk.Application):
+	def __init__(self):
+		Gtk.Application.__init__(self, application_id="com.gabmus.mlauncher", flags=Gio.ApplicationFlags.FLAGS_NONE)
+
+		self.connect("activate", self.activateCb)
+		
+		
+	def do_startup(self):
+	# start the application
+		Gtk.Application.do_startup(self)
+		
+	def activateCb(self, app):
+		window = builder.get_object("window1")
+		window.set_wmclass("mLauncher", "mLauncher")
+		app.add_window(window)
+		appMenu=Gio.Menu()
+		appMenu.append("About", "app.about")
+		appMenu.append("Quit", "app.quit")
+		about_action = Gio.SimpleAction.new("about", None)
+		about_action.connect("activate", self.on_about_activate)
+		app.add_action(about_action)
+		quit_action = Gio.SimpleAction.new("quit", None)
+		quit_action.connect("activate", self.on_quit_activate)
+		app.add_action(quit_action)
+		app.set_app_menu(appMenu)
+		window.show_all()
+		
+	def on_about_activate(self, *agrs):
+		builder.get_object("aboutdialog").show()
+		
+	def on_quit_activate(self, *args):
+		self.quit()
 
 
 class Handler:
@@ -345,8 +379,9 @@ class Handler:
 popover = Gtk.Popover.new(builder.get_object("buttonOpen"))
 popover.add(builder.get_object("openerWidget"))
 builder.connect_signals(Handler())
-window = builder.get_object("window1")
-window.set_wmclass("mLauncher", "mLauncher")
-window.show_all()
 
-Gtk.main()
+
+if __name__ == "__main__":
+	app= App()
+	app.run(sys.argv)
+#Gtk.main()
